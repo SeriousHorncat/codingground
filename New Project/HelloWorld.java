01b3dc7bdb2c2a9dcc754d282c7e8d2bc4e2ba5b
Angelina Uno-Antonison <angelina.antonison@gmail.com>
2015-08-20 11:28:50 -0500

    Created by Coding Ground Client

---
+++ New Project/HelloWorld.java
@@ -0,0 +1,36 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        MovingCat samTheCat = new MovingCat();
+
+        //PRACTICING STATEMENTS
+
+        //! Can change this code below (remove lines, add lines,)
+        //! sam the cat can go up(), down(), left(), right()
+        samTheCat.right();
+        samTheCat.right();
+        samTheCat.left();
+        samTheCat.right();
+
+
+        System.out.println("");
+        System.out.println("---------------");
+        System.out.println("CHANGE DA SPEED!");
+        System.out.println("---------------");
+        System.out.println("");
+
+
+        // PRACTICING STATEMENTS BY CHANGING SPEED
+        // Practice changing the numbers of the statements below
+        //! NOTE:  dx is a way of changing how fast something is moving in the x direction (aka left and right )
+        //         dy is a way of changing how fast something is moving in the y direction (aka up and down )
+      //  samTheCat.DX = 20;
+      //  samTheCat.DY = 20;
+      
+        samTheCat.right();
+        samTheCat.right();
+        samTheCat.left();
+        samTheCat.right();
+    }
+}
